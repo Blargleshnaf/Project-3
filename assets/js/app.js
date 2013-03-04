86ab8097a7d9705279dfb4521b0ca6abb342168f
Andrew Alderson <araukiyo@aim.com>
2013-03-04 16:50:57 -0500

    Added javascript init function
    Added javascript to init jqtouch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
 ;(function($, window) {
+	//init JQTouch please freaking work
+	$(function(){
 	
+	    var jQT = new $.jQTouch({
+	        statusBar: 'black-translucent',
+	        preloadImages: []
+	    });
+
+    });
 }(jQuery, this));
